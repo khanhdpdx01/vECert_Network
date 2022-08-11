ae25598ced1aea653464207841c8f309e16270c6
khanhdpdx01 <tankhanh2k1@gmail.com>
2022-08-11 14:36:14 +0700

    Update connection profile file

--- organizations/ccp-generate.sh
+++ organizations/ccp-generate.sh
@@ -12,6 +12,7 @@ function json_ccp {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
+        -e "s/\${ORGNAME}/$6/" \
         organizations/ccp-template.json
 }
 
@@ -23,23 +24,26 @@ function yaml_ccp {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
+        -e "s/\${ORGNAME}/$6/" \
         organizations/ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
 }
 
 ORG=1
+ORGNAME=issuer
 P0PORT=7051
 CAPORT=1054
 PEERPEM=organizations/peerOrganizations/issuer.com/tlsca/tlsca.issuer.com-cert.pem
 CAPEM=organizations/peerOrganizations/issuer.com/ca/ca.issuer.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/issuer.com/connection-org1.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/issuer.com/connection-org1.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $ORGNAME)" > organizations/peerOrganizations/issuer.com/connection-org1.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $ORGNAME)" > organizations/peerOrganizations/issuer.com/connection-org1.yaml
 
 ORG=2
+ORGNAME=holder
 P0PORT=9051
 CAPORT=8054
 PEERPEM=organizations/peerOrganizations/holder.com/tlsca/tlsca.holder.com-cert.pem
 CAPEM=organizations/peerOrganizations/holder.com/ca/ca.holder.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/holder.com/connection-org2.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/holder.com/connection-org2.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $ORGNAME)" > organizations/peerOrganizations/holder.com/connection-org2.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $ORGNAME)" > organizations/peerOrganizations/holder.com/connection-org2.yaml
--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ CHAIN_CODE_NAME=diploma-management-chaincode
 CHANNEL_NAME=mychannel
 ./network.sh down
 ./network.sh up createChannel -c ${CHANNEL_NAME} -ca
-
+./network.sh deployCC -ccn ${CHAIN_CODE_NAME} -ccp ~/IdeaProjects/${CHAIN_CODE_NAME} -ccl java   
 exec bash
 
